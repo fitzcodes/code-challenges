c5ed7eceb664af354d9612c681b6e0d9d1cd46bf
Thomas Fitzgerald <fitzgerald.thomas.e@gmail.com>
2020-10-15 10:48:25 -0500

    initial commit of 3 assigned questions

---
+++ bigOAssignment.js
@@ -0,0 +1,42 @@
+// Examples of Big O in terms of....
+
+// Constant Time: O(1)
+function random(array) {
+    console.log(array[0]);
+}
+
+random([1, 2, 3, 4, 5, 6]);
+
+
+// Linear Time: O(n)
+function practice(array) {
+    for(let integer = 0; integer < array.length; integer++) {
+        console.log(array[integer]);
+    }
+}
+
+practice([1, 2, 3, 4, 5, 6]);
+
+
+// Polynominal time: O(n^k)
+function wowsers(array) {
+    for(let integer = 0; integer < array.length; integer++) {
+        for(let intTwo = 0; intTwo < array.length; intTwo++) {
+            console.log(array[integer] + array[intTwo]);
+        }
+    }
+}
+
+wowsers([1, 2, 3, 4, 5, 6]);
+
+
+// Extra Assignment
+
+/*  Write a 'circular array' with only a single loop (for, or while).
+    I.E. Create a function that takes 2 parameters: an array, and an integer.
+    - The integer is the number of times the array will be printed.
+    - You must use the mod operator ( % ).
+    - Circular Array
+    - Just console.log the outpout on each loop iteration.
+    - Separate each iteration with ( ----------- ) at this index. */
+
